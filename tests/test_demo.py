8c8f9a9e3e86a71f5aeb7b270a837b2c5df3c21a
Longxing <tanlongxing888@163.com>
2022-10-22 00:18:40 +0800

    refactor: update

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -5,7 +5,7 @@ python -m unittest -v tests/test_demo.py
 import unittest
 
 import tfts
-from tfts import AutoModel, KerasTrainer as Trainer
+from tfts import AutoModel, KerasTrainer as Trainer, build_tfts_model
 
 
 class DemoTest(unittest.TestCase):
@@ -21,6 +21,26 @@ class DemoTest(unittest.TestCase):
 
         pred = trainer.predict(valid[0])
         trainer.plot(history=valid[0], true=valid[1], pred=pred)
+
+    def test_auto_model(self):
+        for m in ["seq2seq", "wavenet", "transformer"]:
+            build_tfts_model(m, predict_length=2)
+
+        for m in [
+            "seq2seq",
+            "wavenet",
+            "transformer",
+            "rnn",
+            "tcn",
+            "bert",
+            "tft",
+            "unet",
+            "informer",
+            "autoformer",
+            "nbeats",
+            "gan",
+        ]:
+            AutoModel(m)
 
     @unittest.skip
     def test_train(self):
