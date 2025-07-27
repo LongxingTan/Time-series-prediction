cd31fadd2a17f80d6fcc7d84d1dd1ad5dd526c76
LongxingTan <tanlongxing888@163.com>
2025-07-26 18:42:59 -0700

    update

--- tfts/models/nbeats.py
+++ tfts/models/nbeats.py
@@ -54,9 +54,9 @@ class NBeats(BaseModel):
 
         # Create custom layers
         self.shape_layer = ShapeLayer()
-        self.squeeze_layer = Lambda(lambda t: tf.squeeze(t, 2))
+        self.squeeze_layer = Lambda(lambda t: tf.squeeze(t, 2), output_shape=lambda s: (s[0], s[1]))
         self.zeros_layer = ZerosLayer(predict_sequence_length)
-        self.expand_dims_layer = Lambda(lambda x: tf.expand_dims(x, -1))
+        self.expand_dims_layer = Lambda(lambda x: tf.expand_dims(x, -1), output_shape=lambda s: s + (1,))
 
         self.block_type = {"trend_block": TrendBlock, "seasonality_block": SeasonalityBlock, "general": GenericBlock}
         self.stacks = []
